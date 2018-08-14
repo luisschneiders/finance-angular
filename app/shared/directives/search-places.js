514d25dd870eccdc7785343549d46508a9e62239
luisfschneiders <schneiders.tech@gmail.com>
2018-08-15 09:59:33 +1000

    Fixed google search address lat and long

--- app/shared/directives/search-places.js
+++ app/shared/directives/search-places.js
@@ -3,34 +3,35 @@ angular.module('MyApp')
     return {
       require: 'ngModel',
       replace: true,
-      $scope: {
+      scope: {
           ngModel: '=',
           latitude: '=',
           longitude: '=',
+          ngDisabled: '='
       },
       template: '<input class="form-control" type="text" placeholder="">',
-      link: function($scope, element, attrs, model) {
+      link: function(scope, element, attrs, model) {
         let options = {
             types: []
         };
 
-        $scope.appStatus = AppStatusServices;
-        $scope.$watch('appStatus.isOnline()', function(online) {
-          $scope.fieldDisabled = online ? false : true;
+        scope.appStatus = AppStatusServices;
+        scope.$watch('appStatus.isOnline()', function(online) {
+          scope.ngDisabled = online ? false : true;
         });
 
         if (typeof google === "undefined") {
-          $scope.appStatus.onLine = false;
+          scope.appStatus.onLine = false;
           return;
         }
 
         let autocomplete = new google.maps.places.Autocomplete(element[0], options);
 
         google.maps.event.addListener(autocomplete, 'place_changed', function() {
-          $scope.$apply(function() {
+          scope.$apply(function() {
             let geoComponents = autocomplete.getPlace();
-            $scope.latitude = geoComponents.geometry.location.lat();
-            $scope.longitude = geoComponents.geometry.location.lng();
+            scope.latitude = geoComponents.geometry.location.lat();
+            scope.longitude = geoComponents.geometry.location.lng();
             model.$setViewValue(element.val());
           });
         });

951cdd949b8ca259aa2361b00d11e1021bdc9de0
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-10-04 19:51:04 +1100

    add controller trip

---
+++ controllers/trip.js
@@ -0,0 +1,49 @@
+const Trip = require('../models/Trip');
+const moment = require('moment');
+
+/**
+ * GET /get-all-trips-month/:year-month
+ */
+exports.getAllTripsByMonth = function(req, res) {
+  let user = req.user.id;
+  let period = req.params.period;
+  let startDate = moment(period).startOf('month').format('YYYY-MM-DD HH:mm:ss');
+  let endDate = moment(period).endOf('month').format('YYYY-MM-DD HH:mm:ss');
+
+  Trip.getAllTripsByMonth(user, startDate, endDate)
+    .then(function(trips) {
+      res.json(trips);
+    }).catch(function(err) {
+      console.error(err);
+    });
+};
+
+/**
+ * POST /trips/new
+ */
+exports.saveTrip = function(req, res) {
+  let errors = null;
+
+}
+
+/**
+ * PUT /trips/remove-trip=id
+ */
+exports.removeTrip = function(req, res) {
+  let trip = null;
+  let checkRecord = new Trip({id: req.params.id});
+
+  if(!checkRecord.isNew()) {
+    trip = new Trip();
+    trip.save({
+      id: req.params.id,
+      tripFlag: 'd',
+    }, { patch: true })
+    .then(function(model) {
+      res.send({ trip: model, msg: 'Trip status has been removed.' });
+    })
+    .catch(function(err) {
+      res.send({ msg: err });
+    });
+  }
+}

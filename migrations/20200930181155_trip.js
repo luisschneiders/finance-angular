e846ad3daa981efde597dfbbcf754bb0869d4c35
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-10-06 19:39:03 +1100

    rename column to tripVehicle in table trip

--- migrations/20200930181155_trip.js
+++ migrations/20200930181155_trip.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('trip', function(table) {
       table.increments();
-      table.integer('tripVehicleId').notNullable();
+      table.integer('tripVehicle').notNullable();
       table.date('tripDate').notNullable();
       table.decimal('tripDistance', [4],[1]);
       table.integer('tripInsertedBy').notNullable();

fba018da82c155620eb5411bffcb58df495e6d3b
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-10-11 10:19:12 +1100

    remove unique for column vehiclePlate in trip table

--- migrations/20201008173513_vehicle.js
+++ migrations/20201008173513_vehicle.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('vehicle', function(table) {
     table.increments();
     table.string('vehicleDescription').notNullable();
-    table.string('vehiclePlate').unique().notNullable();
+    table.string('vehiclePlate').notNullable();
     table.boolean('vehicleIsActive').defaultTo(1);
     table.integer('vehicleInsertedBy').notNullable();
     table.timestamps();

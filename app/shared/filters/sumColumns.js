f6b560e163b711d67ec69a0244a0b32803ef169b
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-10-13 21:40:38 +1100

    use switch statement for sumColumns filter

--- app/shared/filters/sumColumns.js
+++ app/shared/filters/sumColumns.js
@@ -4,12 +4,17 @@
       let total = 0;
 
       _.forEach(collection, function(item) {
-          if (feature == 'timesheet') {
-            if (item.timesheetStatus == condition ) {
+          switch(feature) {
+            case 'timesheet':
+              if (item.timesheetStatus == condition ) {
+                return total += item[key];
+              }  
+              break;
+            case 'purchases':
+            case 'trip':
               return total += item[key];
-            }
-          } else if (feature == 'purchases') {
-            return total += item[key];
+            default:
+              return null;
           }
       });
 
